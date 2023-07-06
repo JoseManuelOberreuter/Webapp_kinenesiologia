b49135cc039c556a735ea47a0930c6871230bdab
Transformer1997 <jose.manuel.oberreuter@gmail.com>
2023-07-06 10:58:48 -0400

    update contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const submitButton = document.getElementById("submitButton");
+const contactForm = document.getElementById("contactForm");
+const submitSuccessMessage = document.getElementById("submitSuccessMessage");
+
+submitButton.addEventListener("click", function() {
+  contactForm.classList.add("d-none");
+  submitSuccessMessage.classList.remove("d-none");
+});
