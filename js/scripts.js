6f20607f1e70e0a962c412a580405ec77173557f
Transformer1997 <jose.manuel.oberreuter@gmail.com>
2023-07-06 11:03:07 -0400

    update contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 const submitButton = document.getElementById("submitButton");
 const contactForm = document.getElementById("contactForm");
 const submitSuccessMessage = document.getElementById("submitSuccessMessage");
+const titulo_form = document.getElementById("titulo_form");
 
-submitButton.addEventListener("click", function() {
+submitButton.addEventListener("submit", function() {
   contactForm.classList.add("d-none");
+  titulo_form.classList.add("d-none");
   submitSuccessMessage.classList.remove("d-none");
 });
